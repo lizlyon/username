5570021787416278c685c9f11bed1611ebbf3fb1
Liz Lyon <elyon@chapman.edu>
2020-11-09 10:44:05 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# username.sh
+# Liz Lyon
+echo "Enter a username: "
+echo "Use lower case characters, digits, and the underscore character"
+echo "Username must start with a lower case character"
+echo "It must contain at least 3 but no more than 12 characters"
+
+read USERNAME
+while echo "$USERNAME" | egrep -v "^[0-9]{5}$" > /dev/null 2>&1
+do
+	echo "You must enter a valid username!"
+	echo "Enter a username: "
+	read USERNAME
+done
+echo "Thank you"
